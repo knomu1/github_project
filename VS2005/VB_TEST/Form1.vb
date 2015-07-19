b67d6c9a4f01f919ef3daf40f2e17d2012a9d13b
nomura <knom1jp@gmail.com>
2015-07-19 15:02:31 +0900

    一部修正

--- VS2005/VB_TEST/Form1.vb
+++ VS2005/VB_TEST/Form1.vb
@@ -1,3 +1,6 @@
 Public Class Form1
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Text = "for GIT Project"
+    End Sub
 End Class
